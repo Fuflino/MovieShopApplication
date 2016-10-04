6b1777eb88a0ad53a9f2a41b126fa7a8513f55bf
NicolaiMensel <nicolai.m.jorgensen@gmail.com>
2016-10-04 08:45:46 +0200

    No changes made

--- MovieShopWepApp/Controllers/AccountController.cs
+++ MovieShopWepApp/Controllers/AccountController.cs
@@ -20,6 +20,7 @@ namespace MovieShopWepApp.Controllers
 
         public AccountController()
         {
+            
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
