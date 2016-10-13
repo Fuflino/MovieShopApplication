c5149608986356be6fce5d93a05b0def06a3f406
NicolaiMensel <nicolai.m.jorgensen@gmail.com>
2016-10-13 11:07:03 +0200

    A new checkout view had been made(not finished) and is ready for testing with movieDetail view.

--- MovieShopDLL/Managers/CustomerManager.cs
+++ MovieShopDLL/Managers/CustomerManager.cs
@@ -69,5 +69,7 @@ namespace MovieShopDLL.Managers
             }
             return false;
         }
+
+        
     }
 }
--- MovieShopWepApp/Controllers/UserController.cs
+++ MovieShopWepApp/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MovieShopDLL;
 using MovieShopDLL.Entities;
+using MovieShopWepApp.Models;
 
 namespace MovieShopWepApp.Controllers
 {
@@ -12,6 +13,7 @@ namespace MovieShopWepApp.Controllers
     {
 
         private IManager<Movie, int> _movieManager = new DLLFacade().GetMovieManager();
+        private IManager<Customer, int> _CustomerManager = new DLLFacade().GetCustomerManager();
 
         // GET: User
         public ActionResult Index()
@@ -22,6 +24,33 @@ namespace MovieShopWepApp.Controllers
         public ActionResult MovieDetails(int id)
         {
             return View(_movieManager.Read(id));
+        }
+
+
+
+        public ActionResult Checkout(int movieId)
+        {
+            var movieToOrder = _movieManager.Read(movieId);
+            var viewModel = new CheckoutViewModel() { customer = null, movie = movieToOrder };
+            return View(viewModel);
+        }
+
+        //GET: Checkout view
+        public ActionResult Checkout(String email, Movie movieToOrder)
+        {
+            var customer = SearchByEmail(email);
+            var viewModel = new CheckoutViewModel() {customer = customer, movie = movieToOrder};
+            return View(viewModel);
+
+        }
+        
+        public Customer SearchByEmail(String email)
+        {
+            foreach (var customer in _CustomerManager.ReadAll())
+            {
+                if (customer.Email.Trim().Equals(email.Trim())) return customer;
+            }
+            return null;
         }
     }
 }
