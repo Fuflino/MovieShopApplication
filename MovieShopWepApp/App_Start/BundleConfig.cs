c02c74d4319892643af119f99bd0e37333c976e8
Fuflino <St@networks.dk>
2016-11-08 09:18:17 +0100

    Stuff

--- MovieShopWepApp/App_Start/BundleConfig.cs
+++ MovieShopWepApp/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace MovieShopWepApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
-                      "~/Styles/UserIndexView.css"));
+                      "~/Styles/UserIndexView.css",
+                      "~/Styles/PartialViewStyleSheet.css"));
         }
     }
 }
